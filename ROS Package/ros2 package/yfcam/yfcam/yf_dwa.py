b75a0e44964a6a49cf8264f0df3af28e91f1a3ef
C'taungdrier <532765732@qq.com>
2020-09-26 11:20:10 +0200

    able to track and run

--- ROS Package/ros2 package/yfcam/yfcam/yf_dwa.py
+++ ROS Package/ros2 package/yfcam/yfcam/yf_dwa.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 # import motorSerial
 import time
 import yaml
+import potential_field_planning
 
 
 def init():
@@ -25,42 +26,8 @@ def init():
 
 def get_target(ziel):
     # ??????
-    target = np.array([ziel.pose.position.x + 2.5, ziel.pose.position.z])
+    target = np.array([ziel.pose.position.x + 2.5 - 0.06, ziel.pose.position.z])
     return target
-
-
-def matplotlib_show(trajectory_soll, dwa, obmap, target, all_trajectory):
-    # ???Matplotlib ????
-    # !!!!!!!!! ???cv2??????
-    trajectory_ist = np.vstack((trajectory_soll, dwa.x))
-
-    # for i in range(10000):
-    #     if obMap[]
-    obmap = dwa.obmap2coordinaten(obmap, 5 / 100)
-    if dwa.show_animation:
-        plt.cla()
-        # for stopping simulation with the esc key.
-        plt.gcf().canvas.mpl_connect(
-            'key_release_event',
-            lambda event: [exit(0) if event.key == 'escape' else None])
-
-        plt.plot(dwa.x[0], dwa.x[1], "xr")
-        plt.plot(obmap[:, 0], obmap[:, 1], "sk")
-        for i in range(len(all_trajectory)):
-            plt.plot(all_trajectory[i][:, 0], all_trajectory[i][:, 1], "-c")
-        # plt.plot(trajectory_soll[:, 0], trajectory_soll[:, 1], "-g")
-        dwa_module.plot_robot(dwa.x[0], dwa.x[1], dwa.x[2], dwa)
-        dwa_module.plot_arrow(dwa.x[0], dwa.x[1], dwa.x[2])
-        plt.plot(trajectory_ist[:, 0], trajectory_ist[:, 1], "-r")
-        plt.plot(target[0], target[1], "^r")
-        plt.axis("equal")
-        plt.grid(True)
-        plt.pause(0.0001)
-        # print('wheel speed is:',wheel_speed)
-        print("Position :", dwa.x[:2])
-        # plt.pause(1)
-        plt.clf()
-        plt.ioff()
 
 
 def wheel_speed_caculator(v_l_rpm, v_r_rpm):
@@ -86,142 +53,172 @@ def main():
 
     # ??????
     init()
-    maps = yf_node.YF_CostMap(nodeName["CostMap"], "CostMap")
-    real = yf_node.YF_RealSpeed(nodeName["RealSpeed"], "RealSpeed")
-    soll = yf_node.YF_SollSpeed(nodeName["SollSpeed"], "SollSpeed")
-    ziel = yf_node.YF_Goal(nodeName['Goal'], 'Goal')
-    flag = yf_node.YF_ObjectFlag(nodeName['ObjectFlag'], 'ObjectFlag')
-    flag.publishMsg(101)
+    maps = yf_node.YF_Image_PY(nodeName["CostMap"], "CostMap", "sub")
+    ziel = yf_node.YF_Goal(nodeName['Goal'], 'Goal', 'sub')
+    real = yf_node.YF_RealSpeed(nodeName["RealSpeed"], "RealSpeed", "sub")
+    soll = yf_node.YF_SollSpeed(nodeName["SollSpeed"], "SollSpeed", "pub")
 
-    # ??????????
     rclpy.spin_once(soll.node, timeout_sec=0.05)  # original 0.001
+    rclpy.spin_once(real.node, timeout_sec=0.05)
+    rclpy.spin_once(maps.node, timeout_sec=0.05)  # original 0.001
+    rclpy.spin_once(ziel.node, timeout_sec=0.05)
 
     # ??DWA???
-    dwa = dwa_module.DWA_Controller()
+    test_own_map = False
+    save_first_frame = True
+    car_state, flag_state = dwa_module.dwa_init()
+    pf_state = potential_field_planning.pf_planner_init()
+    dwa = dwa_module.DWA_Controller(car_state, flag_state)
+    pf_planner = potential_field_planning.Potential_Field_Planner(pf_state)
     old_target = np.array([-1., -1.])
-    SAVE_MAP = True
     # init a time point for fps
-    t = time.time()
-    # FAKE_TARGET = True
-    # fake_target = np.array([2., 4.])
+    start = time.time()
+    u_soll = np.array([0., 0., 0., 0.])
+    car_traj = []
+    if test_own_map:
+        target = np.array([2.5, 4.])
+        oblist = np.load("/home/yf/dev_ws/test_2m2m_map.npy")
+
+        target_traj = pf_planner.potential_field_planning(target, oblist)
+        i_ = 0
+        goal_to_reach = target_traj[0]
+        car_traj = []
+
     while True:
+        # time.sleep(0.1)
+        rclpy.spin_once(soll.node, timeout_sec=0.05)  # original 0.001
+        rclpy.spin_once(real.node, timeout_sec=0.05)
+        rclpy.spin_once(maps.node, timeout_sec=0.05)  # original 0.001
+        rclpy.spin_once(ziel.node, timeout_sec=0.05)
         # ????
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
 
-        rclpy.spin_once(flag.node, timeout_sec=0.01)
-        # subscribe flag signal
-        signal = flag.subMsg.data
+        # print fps
+        # print("fps: ", int(1 / (time.time() - t)))
+        t = time.time()
 
-        if signal == 101:
-            print("Waiting for a Target")
+        # target, oblist = dwa.inverse_transforamtion(target, oblist)
+
+        if real.subMsg is None:
+            print("Waiting for realspeed message")
+            soll.publishMsg([0.0, 0.0, 0.0, 0.0])
             continue
-        if signal.tolist() > 0:
-            # ???????
-            rclpy.spin_once(ziel.node, timeout_sec=0.01)
-            rclpy.spin_once(maps.node, timeout_sec=0.01)
-            rclpy.spin_once(real.node, timeout_sec=0.01)
+        elif maps.subMsg is None:
+            print("Waiting for map message")
+            soll.publishMsg([0.0, 0.0, 0.0, 0.0])
+            continue
+        elif ziel.subMsg is None:
+            print("Waiting for ziel message")
+            soll.publishMsg([0.0, 0.0, 0.0, 0.0])
+            continue
 
-            # ????
-            rclpy.spin_once(soll.node, timeout_sec=0.01)
-
-            # ????
-            obMap = maps.subMsg
-            # print('map before',obMap)
-            obMap = np.resize(obMap, (mapSize, mapSize))
-            goal = ziel.subMsg
-
-            # print fps
-            print("fps: ", int(1 / (time.time() - t)))
-            t = time.time()
-
-            # ????????
-            if goal is None:
-                print("Waiting for new target")
-                continue
-            else:
-                if obMap is None:
-                    print("Waiting for new obMap")
-                    continue
-                # print('current goal:', goal)
-                if not signal == 100:
-                    target = get_target(goal)
-                    if target is not None:
-                        print("target detected")
-                        old_target = target
-                        obMap = dwa.clear_human_shape(target, obMap, 5 / 100)
-                        oblist = dwa.obmap2coordinaten(obMap, 5 / 100)
-                    else:
-                        continue
-                    # if FAKE_TARGET:
-                    #     target = fake_target
-                    print('current target:', target)
-                else:
-                    obMap = dwa.clear_human_shape(old_target, obMap, 5 / 100)
-                    oblist = dwa.obmap2coordinaten(obMap, 5 / 100)
-                    if SAVE_MAP:
-                        np.save("/home/yf/dev_ws/save_map.npy",oblist)
-                        SAVE_MAP = False
-                    old_target, oblist = dwa.inverse_transforamtion(old_target, oblist)
-                    target = old_target
-                    signal = 100
-                    print("static target, start transform map and goal")
-                    dwa.TRANSFORM_MAP = True
-                    if dwa.GOAL_ARRIVAED:
-                        signal = flag.subMsg.data
-
-                # fake_target = np.copy(target)
-                # print('using fake target')
-                # else:
-                #     # dwa.TRANSFORM_MAP = True
-                # print('no transform goal')
-                # print('no update obmap but transform frame')
-
-            if real.subMsg is None:
-                print("Waiting for real_speed and publish soll value [0,0,0,0]")
-                soll.publishMsg([0.0, 0.0, 0.0, 0.0])
-                continue
         else:
-            continue
+            t1 = time.time()
+            # get target
+            target = get_target(ziel.subMsg)
 
-        # ???????
-        real_wheel = real.subMsg
-        # target = np.array([2.5,4.])
-        # real_wheel = [200.,0,200.,0,200.,0,200.]
-        # print("Got real_wheel: ",real_wheel)
-        left_front = real_wheel[0]
-        right_front = real_wheel[2]
-        left_back = real_wheel[4]
-        right_back = real_wheel[6]
-        u_ist = np.array([(left_front + left_back) / 2, (right_front + right_back) / 2])
+            # get wheel speed
+            real_wheel = real.subMsg
+            left_front = real_wheel[0]
+            right_front = real_wheel[2]
+            left_back = real_wheel[4]
+            right_back = real_wheel[6]
+            # print("angle:{:.2f},{:.2f},{:.2f},{:.2f}".format(real_wheel[1],real_wheel[3],real_wheel[5],real_wheel[7]))
+            u_ist = np.array([(left_front + left_back) / 2, (right_front + right_back) / 2])
 
-        dwa.GOAL_ARRIVAED = False
-        dwa.RESET_STATE = True
-        t1 = time.time()
-        # print('oblist shape:',np.shape(oblist))
-        u_soll, trajectory_soll, all_trajectory = dwa.dwa_control(u_ist, dwa.x, target, oblist)
+            # maps transformation
+            obmap = maps.subMsg[:, :, 0]
+            obmap = np.swapaxes(obmap, 0, 1)
+            obmap = np.flip(obmap, axis=1)
+            if save_first_frame:
+                np.save('/home/yf/dev_ws/obmap.npy', obmap)
+                save_first_frame = False
+            obmap[obmap > 125] = 1
 
-        if dwa.MEASURE_TIME:
-            t2 = time.time()
-            print("run time:", t2 - t1)
+            oblist = dwa.obmap2coordinaten(obmap, 5. / 50.)+np.array([-0.06,0.])
+            # oblist = np.array([0.,0.])
+            # dwa.GOAL_ARRIVAED = False
+            # target_traj = pf_planner.potential_field_planning(target, oblist)########################################################change
+            # goal_to_reach = target_traj[1]
+            # print('oblist shape:',np.shape(oblist))
+            u_soll, traj_soll, all_traj = dwa.dwa_control(u_ist, dwa.x, target, oblist)
+            car_traj.append(dwa.x[:2])
 
-        # speed infomation transforamtion
-        u_soll[0] = float(int(u_soll[0]))
-        u_soll[1] = float(int(u_soll[1]))
-        wheel_speed = [u_soll[0], u_soll[1], u_soll[0], u_soll[0]]
-        # wheel_speed = wheel_speed_caculator(u_soll[0],u_soll[1])
-        # # ??Matplotlib????
-        # matplotlib_show(trajectory_soll,dwa,obMap,target,all_trajectory)
+            # if i_ >= len(target_traj):
+            #     goal_to_reach = target
+            # else:
+            #     goal_to_reach = target_traj[i_]
+            #
+            # if dwa.TEMPORARY_GOAL_ARRIVED:
+            #     i_ += 1
+            #     dwa.TEMPORARY_GOAL_ARRIVED = False
+
+            # goal_to_reach, oblist = dwa.inverse_transforamtion(goal_to_reach, oblist, tr_map=True)
+            # target = dwa.inverse_transforamtion(target, oblist, tr_map=False)
+
+            if dwa.SHOW_ANIMATION:
+                plt.cla()
+                figManager = plt.get_current_fig_manager()
+                figManager.window.showMaximized()
+                # for stopping simulation with the esc key.
+                plt.gcf().canvas.mpl_connect(
+                    'key_release_event',
+                    lambda event: [exit(0) if event.key == 'escape' else None])
+
+                plt.plot(dwa.x[0], dwa.x[1], "xr")
+                plt.plot(oblist[:, 0], oblist[:, 1], "sk")
+
+                for i in range(len(all_traj)):
+                    plt.plot(all_traj[i][:, 0], all_traj[i][:, 1], "-c")
+                # print(trajectory_soll)
+                plt.plot(traj_soll[:, 0], traj_soll[:, 1], "-g")
+                dwa_module.plot_robot(dwa.x[0], dwa.x[1], dwa.x[2], dwa)
+                dwa_module.plot_arrow(dwa)
+
+                if dwa.RESET_STATE:
+                    # plt.plot(goal_traj[:, 0], goal_traj[:, 1], color='purple')
+                    # plt.plot(traj_ist[:, 0], traj_ist[:, 1], color='orange')
+                    plt.plot(target_traj[:, 0], target_traj[:, 1], color='orange')
+                    plt.plot(target[0], target[1], 'r')
+                # else:
+                #     plt.plot(traj_ist[:, 0], traj_ist[:, 1], "-r")
+
+                plt.plot(target[0], target[1], "^r")
+                plt.axis("equal")
+                plt.grid(True)
+                plt.pause(0.1)
+
+            if np.linalg.norm(target - dwa.x[:2]) < 1.:
+                dwa.GOAL_ARRIVAED = True
+            # speed infomation transforamtion
+            u_soll[0] = float(int(u_soll[0]))
+            u_soll[1] = float(int(u_soll[1]))
+            wheel_speed = [u_soll[0], u_soll[1], u_soll[0], u_soll[1]]
+
+            # if time.time() - start > 10:
+            np.save('/home/yf/yifan/car_traj.npy', np.array(car_traj))
+            # dwa.GOAL_ARRIVAED = True
+
         if dwa.GOAL_ARRIVAED:
+            print("goal arrived")
             wheel_speed = [0.0, 0.0, 0.0, 0.0]
-            dwa.GOAL_ARRIVAED = False
-        # print("wheel speed: ", wheel_speed)
-        soll.publishMsg(wheel_speed)
-        rclpy.spin_once(ziel.node, timeout_sec=0.01)
+            time.sleep(0.15)
+            # soll.publishMsg(wheel_speed)
+            # rclpy.spin_once(soll.node, timeout_sec=0.05)
+            # dwa.GOAL_ARRIVAED = False
 
+        # if time.time() - t > 10.:
+        #     wheel_speed = [0.0, 0.0, 0.0, 0.0]
+        # print("wheel speed: ", wheel_speed)
+        print('fps = ', 1 / (time.time() - t))
+        soll.publishMsg(wheel_speed)
+
+        if dwa.GOAL_ARRIVAED:
+            dwa.GOAL_ARRIVAED = False
+            # time.sleep(0.3)
         # ??????
-    maps.node.destroy_node()
     soll.node.destroy_node()
-    ziel.node.destroy_node()
     real.node.destroy_node()
+    # plt.show()
